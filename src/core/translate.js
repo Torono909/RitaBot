2aba94976f8ca3be92ff15b9f5488babd04f05d0
2jang <jakeecc@naver.com>
2021-09-10 15:01:30 +0900

    Update translate.js

--- src/core/translate.js
+++ src/core/translate.js
@@ -29,11 +29,10 @@ function discordPatch (string)
    let everyonePing = string.match(/@everyone|@here/giu);
    let urlMatch = string.match(urlRegex);
 
-   const regexFix = string.replace(/<.*?>/gmiu, "<>").
+   const regexFix = string.replace(/<.*?>/gmiu, "").
       replace(urlRegex, "{}").
       replace(/@everyone/g, "[]").
-      replace(/@here/g, "[]").
-      replace(/<@/g, "");
+      replace(/@here/g, "[]");
    if (!urlMatch)
    {
 
